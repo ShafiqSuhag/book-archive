b9d9e5b3949ef80bb198f046ccbdaebea4f03385
Shafiq Suhag <shafiqsuhag@gmail.com>
2021-09-02 02:39:11 +0600

    ui improvement

--- script.js
+++ script.js
@@ -1,73 +1,83 @@
-// variables declration 
+// variables declaration  
 
 const searchInput = document.getElementById('search-input')
 const btnSearch = document.getElementById('btn-search')
+const searchResultContainer = document.getElementById('search-result-container')
+const searchSpinner = document.getElementById('search-spinner')        
+const resultCount = document.getElementById('result-count')
+// init
+searchSpinner.style.display = 'none'
 
 
-btnSearch.addEventListener('click',function(){
-    if(searchInput.value){
+// Event Handler 
+searchInput.addEventListener('keyup',function(event){
+    if(event.key === 'Enter'){
         getDataFromOpenLibrary(searchInput.value);
     }
-    
+})
+
+btnSearch.addEventListener('click',function(){
+    getDataFromOpenLibrary(searchInput.value);
 })
 
 
-// api handle 
-
-
+// api call
 const getDataFromOpenLibrary = (searchValue) => {
+
+    if(searchValue){
+        // Clear old results 
+        searchResultContainer.innerText='';
+        resultCount.innerText = '';
+        searchSpinner.style.display = 'block'
+
+        
+        const url = `https://openlibrary.org/search.json?q=${searchValue}`
+        fetch(url)   
+        .then(res => res.json())
+        .then(data => displayData(data))
+    }
+    else{
+        console.log('Type book name to search ');
+    }
+
     
-    const url = `https://openlibrary.org/search.json?q=${searchValue}`
-    fetch(url)   
-    .then(res => res.json())
-    .then(data => displayData(data))
    
 }
 
+const checkArray = (value) => Array.isArray(value) === true ?  value[0] : value;
 
 
 const displayData = (data) => {
     console.log(data);
-    // console.log(2,data.docs);
-
-
+    
     // booksList 
     const booksList = data.docs;
+    const booksFound = data.numFound; 
+    
+    resultCount.innerText = `(Result Match : ${booksFound} )`
+
     booksList.forEach(book => {
-        // Book Name 
-        const bookName = book.text[2]; 
-        // Author Name
-        const authorName = book.author_name[0];
-        const publisher = book.publisher;
-        const publishingDate = book.first_publish_year;
-        const coverId = book.cover_i
-        // Publisher
-        // publishing date
-        console.log([bookName, authorName, publisher,  publishingDate, book.cover_i])
-        // console.log([bookName, authorName, ...publisher,  publishingDate])
-        // book.key;
-        // console.log(book.text[2])
-
-
-        // generate elements for displaying result 
-        const searchResultContainer = document.getElementById('search-result-container')
-        // console.log(searchResultContainer);
+        
+        const bookName = book.text[2];                                                                               // Book Name 
+        const authorName =  checkArray(book.author_name);                                                           // Author Name
+        const publisher =  checkArray(book.publisher);                                                             // Publisher
+        const publishingDate = checkArray(book.first_publish_year);                                               // publishing date
+        const coverId = book.cover_i                                                                             // book cover image
+        
+        
+        // console.log([bookName, authorName, publisher,  publishingDate, book.cover_i])
+        
 
         const bookDiv  = document.createElement('div')
-        bookDiv.classList.add('col-4')
+        bookDiv.classList.add('col-md-4')
         bookDiv.classList.add('p-3')
-        // bookDiv.classList.add('bg-info')
         
-        // bookDiv.classList.add('shadow-lg')
-        // bookDiv.classList.add('rounded')
-        
-        //  bg-info p-3  shadow-lg  rounded
         bookDiv.innerHTML = 
         `
     
-        <div class="row bg-info shadow-lg rounded">
-            <div class="col-md-6 p-2">
-                <img class="rounded" src="https://covers.openlibrary.org/w/id/${coverId}-M.jpg" alt="" width="180px" height="290px">
+        <div class="row  shadow-lg rounded">
+            <div class="col-md-6 p-2 d-flex justify-content-center align-items-center">
+                <img class="border rounded" src="https://covers.openlibrary.org/w/id/${coverId}-M.jpg" alt="image not found" width="180px" height="290px">
             </div>
             <div class="col-md-6 d-flex flex-column justify-content-center">
                 <p> <b>Book Name :</b>  ${bookName} </p>
@@ -80,23 +90,7 @@ const displayData = (data) => {
         </div>
     
         `;
-
         searchResultContainer.appendChild(bookDiv) ;
-
-
-
     });
-    // const bookLoop = booksList =>booksList.forEach(element => {
-        
-    // });
-
-   
-
+    searchSpinner.style.display = 'none' 
 }
-
-// getDataFromOpenLibrary();
-
-
-// const test = ['one','two']
-
-// console.log(...test)
